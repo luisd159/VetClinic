adee7986ed04795e00b44ec3738fb34f60aaeb60
Luis Eduardo Avila <crazylora123@gmail.com>
2019-11-10 12:38:15 -0500

    CRUD

--- app/controllers/historials_controller.rb
+++ app/controllers/historials_controller.rb
@@ -3,5 +3,50 @@ class HistorialsController < ApplicationController
       @historials = Historial.all
     end
     def new
+      @historial = Historial.new
     end
+    def create
+      @historial = Historial.new(historial_params)
+      if @historial.save
+        flash[:notice] = "Historial was successsfully created"
+        redirect_to historials_path
+        
+      else
+        render :new
+      end
+    end
+
+    def edit
+      @historial = Historial.find(params[:id])
+    end
+
+    def update
+      @historial = Historial.find(params[:id])
+      if @historial.update(historial_params)
+        flash[:notice] = "Historial was successsfully updated"
+        redirect_to historials_path
+        
+      else
+        render :new
+      end
+    end
+
+    def destroy
+      @historial = Historial.find(params[:id])
+      @historial.destroy
+      flash[:notice] = "Historial was successsfully deleted"
+        redirect_to historials_path
+    end
+
+    def show
+      @historial = Historial.find(params[:id])
+    end
+
+    private 
+
+    def historial_params
+      params.require(:historial).permit(:owner_name, :consult_reason)
+    end
+
+    
 end
